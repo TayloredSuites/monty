a02a95cb32cd83af57e7555f7d674e79088da97f
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-26 10:52:49 -0700

    Created files

---
+++ monty.c
@@ -0,0 +1,19 @@
+if (argv < 1 || argv == NULL)
+{
+	fprintf("USAGE: monty ./monty.c\n");
+	exit(EXIT_FAILURE);
+}
+read(monty.c, RONLY|truncate);
+fprintf("Error: Can't open file $file_name\n");
+exit(EXIT_FAILURE);
+if ($file_name != valid_instruc)
+{
+	fprintf("L<line_number>: unknown instruction <opcode>\n");
+	exit(EXIT_FAILURE);
+	line_number = 1;
+	line_number = argc;
+
+	if (!malloc)
+	{
+	fprintf("Error: malloc failed\n");
+	exit(EXIT_FAILURE);
